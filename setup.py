fbd7f180a3d2ad036f932873665c7b8894a66a71
jlpl <janne.lampilahti@gmail.com>
2022-05-10 20:33:20 +0300

    Update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="smear-api-tools",
-    version="0.2.1",
+    version="0.2.2",
     description='Utility functions for using the SmartSMEAR API',
     py_modules=["smear_api_tools"],
     package_dir={'':'src'},
