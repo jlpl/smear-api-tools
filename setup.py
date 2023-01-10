14b0c38fe851b171a6ed9b13d75af47aafc93ada
jlpl <janne.lampilahti@gmail.com>
2023-01-10 22:05:41 +0200

    Remove conc and cs functions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="smear-api-tools",
-    version="0.2.2",
+    version="0.2.4",
     description='Utility functions for using the SmartSMEAR API',
     py_modules=["smear_api_tools"],
     package_dir={'':'src'},
